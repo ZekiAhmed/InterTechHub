3d25b65b23468d5898682c7ace264566a8b58521
ZekiAhmed <changezworld3@gmail.com>
2024-11-16 12:55:36 -0800

    created three routes /name, /hobby and /dream

--- bke_s1_api/index.js
+++ bke_s1_api/index.js
@@ -2,6 +2,18 @@ import express from "express";
 
 const app = express();
 
+const hobby = ["Reading book", "Walking", "Riding horse"];
+
+app.get("/name", (req, res) => {
+  res.status(200).send("Zeki Ahmed");
+});
+app.get("/hobby", (req, res) => {
+  res.status(200).json(hobby);
+});
+app.get("/dream", (req, res) => {
+  res.status(200).json({ message: "I want to be senior backend engineer." });
+});
+
 app.listen(3000, () => {
   console.log("Backend Server Is Runing!!");
 });
