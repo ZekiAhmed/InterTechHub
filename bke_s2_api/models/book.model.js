d140be9b151ef136fd8fad8409557fba3e1c4633
ZekiAhmed <changezworld3@gmail.com>
2024-11-23 17:33:23 -0800

    created BookSchema for Book model

---
+++ bke_s2_api/models/book.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const BookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "title is required"],
+    },
+    author: {
+      type: String,
+      required: [true, "author is required"],
+    },
+    isbn: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          return /\d{5}/.test(v);
+        },
+        message: (props) =>
+          `${props.value} is not a valid isbn number, it has to be 5 digits!`,
+      },
+      required: [true, "Isbn is required"],
+      unique: true,
+    },
+    favorite: {
+      type: Boolean,
+    },
+    pubYear: {
+      type: Number,
+      required: [true, "published year is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Book", BookSchema);
