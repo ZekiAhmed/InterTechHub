71700fe6e02955f924bb4ad391784dc932398ef3
ZekiAhmed <changezworld3@gmail.com>
2024-12-07 14:59:20 -0800

    auth routes file with the route  is created

---
+++ bke_s3_api/routes/book.route.js
@@ -0,0 +1,39 @@
+import express from "express";
+import {
+  apiUsageGuide,
+  bookRecommendation,
+  deleteBook,
+  favoriteBook,
+  findAllBook,
+  findBook,
+  saveBook,
+  updateBook,
+} from "../controllers/book.controller.js";
+
+const router = express.Router();
+
+// CREATE BOOK
+router.post("/", saveBook);
+
+// UPDATE BOOK
+router.put("/:id", updateBook);
+
+// DELETE BOOK
+router.delete("/:id", deleteBook);
+
+// GET BOOK
+router.get("/", findBook);
+
+// GET ALL BOOKS
+router.get("/all", findAllBook);
+
+// GET SOME BOOK RECOMMENDATION
+router.get("/recommendation", bookRecommendation);
+
+// ADD BOOK AS FAVORITE
+router.get("/favorite/:id", favoriteBook);
+
+// API USAGE GUIDE
+router.get("/guide", apiUsageGuide);
+
+export default router;
